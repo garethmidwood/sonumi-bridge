2959ee9a6d980bde86e54abd91a7181c46852365
Gareth Midwood <gmidwood@inviqa.com>
2016-04-14 18:56:59 +0100

    Add try/catch wrapper

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var app    = require('./lib/sonumi');
+var sonumiLogger = require('sonumi-logger');
+var config = require('config');
 
-app();
+var logDirectory = config.logging.logDir;
+
+logger = sonumiLogger.init(logDirectory);
+logger.addLogFile('info', logDirectory + '/sonumi-bridge-info.log', 'info');
+logger.addLogFile('errors', logDirectory + '/sonumi-bridge-errors.log', 'error');
+
+try {
+    // off we go
+    app();
+} catch (err) {
+    logger.error('Failed! Error!');
+    logger.error(err);
+    process.exit(1);
+}
